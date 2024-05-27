078c0b8b2508b5e1b45e40ea6ac5d6639edd0298
kulaizki <kulaizke@gmail.com>
2024-05-27 14:35:10 +0800

    refactor: test container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
+        <div className="h1 text-5xl font-bold">izk</div>
         <h1 className='text-3xl font-bold'>Insight</h1>
       </div>
     </section>
