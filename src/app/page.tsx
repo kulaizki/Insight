cf93f9c56c4ff80631638375a663b7f288e692c1
kulaizki <kulaizke@gmail.com>
2024-06-10 21:18:42 +0800

    feat: add test paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <div className='container'>
         <div className="h1 text-5xl font-bold">izk</div>
         <h1 className='text-3xl font-bold'>Insight</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quae architecto deserunt quam. Nobis autem assumenda optio porro neque ex! Porro, labore ab doloribus quibusdam id nam dolor dolores tempore.</p>
       </div>
     </section>
   )
