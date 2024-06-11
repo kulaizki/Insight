10a6909e83f32e525a454e5f1c016591229da9b7
kulaizki <kulaizke@gmail.com>
2024-06-11 11:58:56 +0800

    feat: add some responsiveness to landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <div className='flex gap-8 items-center border-4 p-8'>
+    <div className="flex justify-center p-4 md:p-8">
+      <div className='flex flex-col md:flex-row gap-4 md:gap-8 items-center border-4 p-4 md:p-8'>
         <div className='flex flex-col gap-4'>
-          <h1 className='text-6xl font-bold'>Insight</h1>
-          <p className='max-w-xl text-lg'>
+          <h1 className='text-4xl md:text-6xl font-bold'>Insight</h1>
+          <p className='leading-7 max-w-full md:max-w-xl text-base md:text-lg'>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quae
             architecto deserunt quam. Nobis autem assumenda optio porro neque
             ex! Porro, labore ab doloribus quibusdam id nam dolor dolores
             tempore.
           </p>
         </div>
-        <img className='rounded-md' src='./maru-red.jpeg' alt='maru' />
+        <img className='rounded-md w-full md:w-auto' src='./maru-red.jpeg' alt='maru' />
       </div>
     </div>
   )
