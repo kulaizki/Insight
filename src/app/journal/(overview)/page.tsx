f3743c994a61bed5efd4caad6692274c846bd8f3
kulaizki <kulaizke@gmail.com>
2024-06-19 21:13:49 +0800

    nit: add comment for wanted feature

--- src/app/journal/(overview)/page.tsx
+++ src/app/journal/(overview)/page.tsx
@@ -8,6 +8,7 @@ export default async function Page() {
       <div className='flex flex-col p-8 gap-4'>
         <h1 className='text-6xl font-bold'>Journal</h1>
         <p className="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quae architecto deserunt quam. Nobis autem assumenda optio porro neque ex! Porro, labore ab doloribus quibusdam id nam dolor dolores tempore.</p>
+        {/* add trade table here, and manually input data */}
       </div>
     </section>
   )
