c4aa2b4f857483b6226c6c8039b1cb440b1cbe4d
kulaizki <kulaizke@gmail.com>
2024-06-21 21:48:42 +0800

    feat: add resources route folder

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,22 +1,22 @@
 const resources: { title: string; href: string; description: string }[] = [
   {
     title: 'Price Action',
-    href: '/price-action',
+    href: '/resources/price-action',
     description: 'Trend Lines, Patterns, and Volume'
   },
   {
     title: 'Smart Money Concept',
-    href: '/smart-money-concept',
+    href: '/resources/smart-money-concept',
     description: 'Order Blocks, Imbalances, and Liquidity'
   },
   {
     title: 'Harmonic Patterns',
-    href: '/harmonic-patterns',
+    href: '/resources/harmonic-patterns',
     description: 'Pattern Structure, Ratios, and Targets'
   },
   {
     title: 'Trading Psychology',
-    href: '/trading-psychology',
+    href: '/resources/trading-psychology',
     description: 'Risk Management, Discipline, and Patience'
   }
 ]
