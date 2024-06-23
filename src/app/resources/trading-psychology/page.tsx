b37eb2d89f0d233af306d1e78acfa67e4bc6aae9
kulaizki <kulaizke@gmail.com>
2024-06-23 16:54:27 +0800

    style: remove vertical padding

--- src/app/resources/trading-psychology/page.tsx
+++ src/app/resources/trading-psychology/page.tsx
@@ -2,7 +2,7 @@
 
 export default async function Page() {
   return (
-    <section className='py-24'>
+    <section>
       <div className='flex flex-col gap-4 p-8'>
         <h1 className='text-6xl font-bold'>Trading Psychology</h1>
         <p className='text-lg'>
