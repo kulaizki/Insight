e765bda98a06522840b4ece7e672e4725fdf9bc1
kulaizki <kulaizke@gmail.com>
2024-06-23 22:15:37 +0800

    feat: add footer responsiveness for smaller screens

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,14 +2,17 @@
 
 import * as React from 'react'
 import Image from 'next/image'  
+import { useMediaQuery } from 'react-responsive'
 import { socialLinks } from '@/lib/data'
 
 export function Footer() {
+  const isSmallScreen = useMediaQuery({ maxWidth: 480 })
+
   return (
     <div className='flex items-center justify-center gap-4 '>
       {socialLinks.map((link) => (
         <a key={link.href} href={link.href} target='_blank' rel='noopener noreferrer'>
-          <Image src={link.imgSrc} alt={link.alt} width={45} height={45} />
+          <Image src={link.imgSrc} alt={link.alt} width={isSmallScreen ? 30 : 45} height={isSmallScreen ? 30 : 45} />
         </a>
       ))}
     </div>
