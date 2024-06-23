45193976890d61e7ebd1826e2fa3492ef29eb995
kulaizki <kulaizke@gmail.com>
2024-06-23 17:07:58 +0800

    style: remove vertical padding

--- src/app/journal/(overview)/page.tsx
+++ src/app/journal/(overview)/page.tsx
@@ -4,7 +4,7 @@ import { Table } from "@/components/ui/table";
 
 export default async function Page() {
   return (
-    <section className='py-24'>
+    <section>
       <div className='flex flex-col p-8 gap-4'>
         <h1 className='text-6xl font-bold'>Journal</h1>
         <p className="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quae architecto deserunt quam. Nobis autem assumenda optio porro neque ex! Porro, labore ab doloribus quibusdam id nam dolor dolores tempore.</p>
