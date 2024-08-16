7e962eefadf270232e8f74d152c62db05fd418a3
kulaizki <kulaizke@gmail.com>
2024-08-16 10:36:47 +0800

    feat: add realtime charts

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets'
 
 export default function Page() {
-
   return (
     <section className='md:px-24'>
       <div className='flex flex-col gap-4 p-4 sm:p-8'>
         <h1 className='text-4xl font-bold sm:text-6xl'>Charts</h1>
-        <p className='max-w-full text-sm sm:text-lg '>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quae
-          architecto deserunt quam. Nobis autem assumenda optio porro neque ex!
-          Porro, labore ab doloribus quibusdam id nam dolor dolores tempore.
-        </p>
+        <div className='mt-4 h-96'>
+          <AdvancedRealTimeChart theme='dark' autosize></AdvancedRealTimeChart>
+        </div>
       </div>
     </section>
   )
