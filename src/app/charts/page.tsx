4058dcb3c2817af2ac9b6a6ab16a9ae9c6ff2dd9
kulaizki <kulaizke@gmail.com>
2024-09-02 21:27:59 +0800

    style: use space instead of margin

--- src/app/charts/page.tsx
+++ src/app/charts/page.tsx
@@ -5,9 +5,9 @@ import { AdvancedRealTimeChart } from 'react-ts-tradingview-widgets'
 export default function Page() {
   return (
     <section className='md:px-24'>
-      <div className='flex flex-col gap-4 p-4 sm:p-8'>
+      <div className='flex flex-col gap-4 p-4 sm:p-8 space-y-4'>
         <h1 className='text-4xl font-bold sm:text-6xl'>Charts</h1>
-        <div className='mt-4 h-96'>
+        <div className='h-96'>
           <AdvancedRealTimeChart theme='dark' autosize></AdvancedRealTimeChart>
         </div>
       </div>
